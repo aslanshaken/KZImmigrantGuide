f8b856c7d4cf1bae5d47feff6bf21622042b958a
asikegoi <aslanshaken@gmail.com>
2021-03-09 18:06:13 -0500

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,75 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
+import Layout from './share/layout/Layout'
+// import Login from './screens/Login';
+import { loginUser, registerUser, verifyUser, removeToken } from './services/auth';
+// import Register from './screens/Register';
+import MainContainer from './containers/MainContainer';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [error, setError] = useState(null);
+  const history = useHistory();
+
+  // useEffect(() => {
+  //   const handleVerify = async () => {
+  //     const currentUser = await verifyUser();
+  //     setCurrentUser(currentUser)
+  //   }
+  //   handleVerify();
+  // }, [])
+
+  // const handleLogin = async (formData) => {
+  //   try {
+  //     const currentUser = await loginUser(formData);
+  //     setCurrentUser(currentUser);
+  //     setError(null);
+  //     history.push('/');
+  //   } catch (e) {
+  //     setError("invalid login credentials");
+  //   }
+  // }
+
+  // const handleRegister = async (formData) => {
+  //   try {
+  //     const currentUser = await registerUser(formData);
+  //     setCurrentUser(currentUser);
+  //     history.push('/');
+  //   } catch (e) {
+  //     setError("invalid sign up info")
+  //   }
+  // }
+
+  // const handleLogout = () => {
+  //   setCurrentUser(null);
+  //   localStorage.removeItem('authToken');
+  //   removeToken();
+  // }
+
   return (
-    <div className="App">
-    
-    </div>
+    <Layout
+      // currentUser={currentUser}
+      // handleLogout={handleLogout}
+    >
+      <Switch>
+        {/* <Route path='/login'>
+          <Login
+            handleLogin={handleLogin}
+            error={error}
+          />
+        </Route>
+        <Route path='/register'>
+          <Register handleRegister={handleRegister} />
+        </Route> */}
+        <Route path='/'>
+          <MainContainer currentUser={currentUser} />
+        </Route>
+
+      </Switch>
+
+    </Layout>
   );
 }
 
---
+++ client/src/containers/MainContainer.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory, Redirect } from 'react-router-dom';
+
+
+export default function MainContainer(props) {
+    return (
+        <div>
+            <h1>Main</h1>
+        </div>
+    )
+}
