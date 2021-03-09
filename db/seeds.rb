a4d91f95faf065517ba98d716fc3c19703f99dfa
asikegoi <aslanshaken@gmail.com>
2021-03-09 10:15:36 -0500

    finished seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,11 @@ User.destroy_all
 
 @aslan = User.create!(username: "aslan", email:"aslan@gmail.com", password: "123456")
 @bob = User.create!(username: "bob", email:"bob@gmail.com", password: "123456")
+@alan = User.create!(username: "alan", email:"alan@gmail.com", password: "123456")
+@john = User.create!(username: "john", email:"john@gmail.com", password: "123456")
+@vanessa = User.create!(username: "vanessa", email:"vanessa@gmail.com", password: "123456")
+@st = User.create!(username: "st", email:"st@mail.com", password: "123456")
+
 
 puts "#{User.count} users created"
 
@@ -45,16 +50,70 @@ GetEmployee.create!(
     job_name: 'FANTASTIC Restaurant MANAGER',
     category:'restaurant', 
     description:'Please have 2 years experience in fine dining management.Familiarity with French American cuisine and wine is helpful.Proficiency with POS a must.', 
-    city: 'New-York',
+    city: 'New-Jersey',
     cellphone: 3475829436,
-    email: 'twentyfivenyc@outlook.com'
+    email: 'twentyfivenj@outlook.com'
     user: @bob
 )GetEmployee.create!(
-    job_name: 'CDL Drivers',
-    category:'drivers', 
-    description:'Non-emergency medical transportation - Transporting clients to and from medical appointments/ Dialysis.', 
-    city: 'New-York',
-    cellphone: 3475769090,
-    email: 'cdldriversnyc@yahoo.com'
+    job_name: 'Production assistant',
+    category:'entertainment', 
+    description:'Manhattan based film / tv production company seeking a production assistant . Must be driven , personable , general knowledge of the industry , willing to travel if necessary. ', 
+    city: 'New-Jersey',
+    cellphone: 9586395685,
+    email: 'filmontheway@yahoo.com'
     user: @aslan
+)
+#==================================================================================
+PostByEmployee.create!(
+    name: 'Alan',
+    about:'A software engineer with 2 years of experiance.', 
+    category:'software engineer', 
+    city: 'New-Jersey',
+    cellphone: 9299289595,
+    email: 'alannyc@gmail.com'
+    user: @alan
+)PostByEmployee.create!(
+    name: 'John',
+    about:'FREELANCE Bookkeeper available P/T .', 
+    category:'bookkeeper', 
+    city: 'New-York',
+    cellphone: 9284568565,
+    email: 'johnbookkeeper@yahoo.com'
+    user: @john
+)PostByEmployee.create!(
+    name: 'Vanessa',
+    about:'A babysitter/Nanny looking for work.', 
+    category:'babysitter', 
+    city: 'New-York',
+    cellphone: 9347565896,
+    email: 'vanessabb@gmail.com'
+    user: @vanessa
+)PostByEmployee.create!(
+    name: 'St',
+    about:'I have over seventeen years of experience as a personal Driver.', 
+    category:'driver', 
+    city: 'New-York',
+    cellphone: 3475966085,
+    email: 'st90@mail.com'
+    user: @st
+)
+#==================================================================================
+PostHouse.create!(
+    name: '1BR HIGH END BY A C TRAINS * LAUNDRY & ROOF * NO FEE',
+    description:'Massive 1Br/1Ba high end apt with tons of space ,huge living room and Office/Baby Room in the apt ,tons of closet space ', 
+    city: 'New-York',
+    data_move_in: 05/02/2021 #??? integer 
+    price: 2000, 
+    cellphone: 3475966085,
+    email: 'ghyb91@mail.com'
+    user: @aslan
+)PostHouse.create!(
+    name: 'Sun-filled railroad apt w/ bedroom + office/nursery AND outdoor patio',
+    description:'Sun-filled 1.5-bedroom railroad apartment on 2nd floor of a three story, owner-occupied, brownstone in South Park Slope, featuring private 15 x 15 outdoor patio.', 
+    city: 'New-Jersey',
+    data_move_in: 04/02/2021 #??? integer 
+    price: 1800, 
+    cellphone: 3478589699,
+    email: 'bob190@mail.com'
+    user: @bob
 )
