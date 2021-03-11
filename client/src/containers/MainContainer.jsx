53eec31bc378ec1c08faabf8589bfeaa71c23134
asikegoi <aslanshaken@gmail.com>
2021-03-11 11:30:13 -0500

    fully working web and cell

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-import { Switch, Route, useHistory, Redirect } from 'react-router-dom';
+
 import Cities from '../components/Cities/Cities';
 import Events from '../components/Events/Events';
 
--- client/src/share/header/Header.jsx
+++ client/src/share/header/Header.jsx
@@ -2,81 +2,171 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './Header.css'
 import Logo from '../../assets/logo.png'
+import { useState } from 'react'
 
 export default function Header(props) {
     const { currentUser, handleLogout } = props;
-    console.log(currentUser)
+    const [close, setClose] = useState(false)
     return (
-        <div className="nav-container">
-            <div>
-                <Link to="/" id="none">
-                    <img id="logo" src={Logo} />
+        <div>
 
-                </Link>
-            </div>
-            <div>
-                <Link to="/jobs" id="none">
-                    <h4 >Jobs</h4>
-                </Link>
-            </div>
-            <div>
-                <Link to="#" id="none">
-                    <h4>Housing</h4>
-                </Link>
-            </div>
-            <div>
-                <Link to="#" id="none">
-                    <h4>Communities</h4>
-                </Link>
-            </div>
-            <div>
-                <Link to="#" id="none">
-                    <h4>Blog</h4>
-                </Link>
-            </div>
-            <div>
-                <Link to="#" id="none">
-                    <h4>Contact</h4>
-                </Link>
-            </div>
+            <div className="nav-container">
+                <div>
+                    <Link to="/" id="none">
+                        <img id="logo" src={Logo} />
 
-            {
-                currentUser ?
-                    <>
-                        <div>
-                            <Link to="/account" id="none">
-                                <h4>My Account</h4>
-                            </Link>
-                        </div>
-                        <div>
-                            <h4 id="logout" onClick={handleLogout}>Sign out</h4>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <div>
-                            <Link to="/login" id="none">
-                                <h4>Sign in</h4>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/register" id="none">
-                                <h4>Sign up</h4>
-                            </Link>
-                        </div>
-                    </>
-            }
-
-            <div>
-                {currentUser ?
-                    <Link to="/create-posts" id="none">
-                        <h4 className="create-post-button">Create a Post</h4>
                     </Link>
-                    :
-                    <h4 className="create-post-button" onClick={()=>alert("Please Sign in or Register")}>Create a Post</h4>
+                </div>
+                <div>
+                    <Link to="/jobs" id="none">
+                        <h4 >Jobs</h4>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="#" id="none">
+                        <h4>Housing</h4>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="#" id="none">
+                        <h4>Communities</h4>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="#" id="none">
+                        <h4>Blog</h4>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="#" id="none">
+                        <h4>Contact</h4>
+                    </Link>
+                </div>
+                {
+                    currentUser ?
+                        <>
+                            <div>
+                                <Link to="/account" id="none">
+                                    <h4>My Account</h4>
+                                </Link>
+                            </div>
+                            <div>
+                                <h4 id="logout" onClick={handleLogout}>Sign out</h4>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <div>
+                                <Link to="/login" id="none">
+                                    <h4>Sign in</h4>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/register" id="none">
+                                    <h4>Sign up</h4>
+                                </Link>
+                            </div>
+                        </>
+                }
+
+                <div>
+                    {currentUser ?
+                        <Link to="/create-posts" id="none">
+                            <h4 className="create-post-button">Create a Post</h4>
+                        </Link>
+                        :
+                        <h4 className="create-post-button" onClick={() => alert("Please Sign in or Register")}>Create a Post</h4>
+                    }
+                </div>
+
+            </div >
+
+            {/* CELL PHONE */}
+            <div className="nav-cell-container" >
+                <div>
+                    <Link to="/" id="none">
+                        <img id="logo" src={Logo} />
+
+                    </Link>
+                </div>
+
+                <img
+                    id="nav-img"
+                    onClick={() => setClose(!close)}
+                    src={close
+                        ? "https://img.icons8.com/ios/50/000000/circled-x.png"
+                        : "https://img.icons8.com/cotton/64/000000/menu.png"
+                    }
+                />
+                {close &&
+                    <>
+
+                        <div id="xs">
+                            <Link to="/jobs" id="none">
+                                <h4 >Jobs</h4>
+                            </Link>
+                        </div>
+                        <div id="xs">
+                            <Link to="/" id="none">
+                                <h4>Housing</h4>
+                            </Link>
+                        </div>
+                        <div id="xs">
+                            <Link to="/" id="none">
+                                <h4>Communities</h4>
+                            </Link>
+                        </div>
+                        <div id="xs">
+                            <Link to="/" id="none">
+                                <h4>Blog</h4>
+                            </Link>
+                        </div>
+                        <div id="xs">
+                            <Link to="/" id="none">
+                                <h4>Contact</h4>
+                            </Link>
+                        </div>
+                        {
+                            currentUser ?
+                                <>
+                                    <div id="xs">
+                                        <Link to="/account" id="none">
+                                            <h4>My Account</h4>
+                                        </Link>
+                                    </div>
+                                    <div id="xs">
+                                        <h4 id="logout" onClick={handleLogout}>Sign out</h4>
+                                    </div>
+                                </>
+                                :
+                                <>
+                                    <div id="xs">
+                                        <Link to="/login" id="none">
+                                            <h4>Sign in</h4>
+                                        </Link>
+                                    </div>
+                                    <div id="xs">
+                                        <Link to="/register" id="none">
+                                            <h4>Sign up</h4>
+                                        </Link>
+                                    </div>
+                                </>
+                        }
+                        <div id="xs">
+                            {currentUser ?
+                                <Link to="/create-posts" id="none">
+                                    <h4 className="create-post-button">Create a Post</h4>
+                                </Link>
+                                :
+                                <h4 className="create-post-button" onClick={() => alert("Please Sign in or Register")}>Create a Post</h4>
+                            }
+                        </div>
+                    </>
                 }
 
             </div>
+
+
 
         </div>
     )
