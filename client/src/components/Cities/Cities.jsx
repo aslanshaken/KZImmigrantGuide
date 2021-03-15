e0ec7ffae72bd142970486e0dda8802eca5b0617
asikegoi <aslanshaken@gmail.com>
2021-03-15 19:34:25 -0400

    updated  images

--- client/src/components/Cities/Cities.jsx
+++ client/src/components/Cities/Cities.jsx
@@ -14,7 +14,7 @@ export default function Cities() {
                         <h4>New York</h4>
                         <div className="">
                             <Link to="#">
-                                <img alt="newyork" src="https://www.teahub.io/photos/full/126-1264161_new-york.jpg" />
+                                <img alt="newyork" src="https://www.bazar.club/wp-content/uploads/1-13.jpg" />
                             </Link>
                         </div>
                     </div>
@@ -23,7 +23,7 @@ export default function Cities() {
                         <h4> New Jersey</h4>
                         <div>
                             <Link to="#">
-                                <img alt="newjersey" src="https://media.vanityfair.com/photos/5b1ecd3d6163e4725653ca9f/master/pass/Newark-Survelliance-Cameras.jpg" />
+                                <img alt="newjersey" src="https://www.bazar.club/wp-content/uploads/2020/03/new_jersey-1024x768.jpeg" />
                             </Link>
                         </div>
                     </div>
@@ -32,7 +32,7 @@ export default function Cities() {
                         <h4> Philadelphia</h4>
                         <div>
                             <Link to="#">
-                                <img alt="phila" src="https://i.pinimg.com/originals/f3/21/62/f32162326aa501697b50ada17596fdea.jpg" />
+                                <img alt="phila" src="https://www.bazar.club/wp-content/uploads/phila.jpg" />
                             </Link>
                         </div>
                     </div>
@@ -41,7 +41,7 @@ export default function Cities() {
                         <h4>Los Angeles</h4>
                         <div>
                             <Link to="#">
-                                <img alt="la" src="http://d3e1m60ptf1oym.cloudfront.net/6e462002-8b45-11e3-a082-259c7e2bfdd9/MG5231-Los-Angeles-Buildings-Black-And-White_xgaplus.jpg" />
+                                <img alt="la" src="https://www.bazar.club/wp-content/uploads/los-ang.jpg" />
                             </Link>
                         </div>
                     </div>
@@ -50,7 +50,7 @@ export default function Cities() {
                         <h4>Miami</h4>
                         <div>
                             <Link to="#">
-                                <img alt="miami" src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/miami-florida-skyline-miami-beach-black-and-white-toby-mcguire.jpg" />
+                                <img alt="miami" src="https://www.bazar.club/wp-content/uploads/miyami.jpg" />
                             </Link>
                         </div>
                     </div>
@@ -59,7 +59,7 @@ export default function Cities() {
                         <h4>Chicago</h4>
                         <div>
                             <Link to="#">
-                                <img alt="chicago" src="http://d3e1m60ptf1oym.cloudfront.net/203e2f6c-4f09-4837-ae58-bc82fa049cbc/MG3986-Chicago-River-Skyline-Night_xgaplus.jpg" />
+                                <img alt="chicago" src="https://www.bazar.club/wp-content/uploads/2020/04/chicago_1.jpg" />
                             </Link>
                         </div>
                     </div>
