2db32fa850c1438150d567dc85b628506b2eccab
asikegoi <aslanshaken@gmail.com>
2021-03-15 21:12:19 -0400

    Added master.key

---
+++ client/src/services/dist/api-config.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _axios = _interopRequireDefault(require("axios"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var baseUrl = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/kz-immigrant-api.git' : 'http://localhost:3000';
+
+var api = _axios["default"].create({
+  baseURL: baseUrl
+});
+
+var _default = api;
+exports["default"] = _default;
