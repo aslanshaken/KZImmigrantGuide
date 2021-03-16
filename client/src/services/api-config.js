a945f28fc5ffa0458c82416e925c263a895bb047
asikegoi <aslanshaken@gmail.com>
2021-03-15 23:21:15 -0400

    working heroku

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/kz-immigrant-api.com' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://kz-immigrant-api.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
     baseURL: baseUrl
