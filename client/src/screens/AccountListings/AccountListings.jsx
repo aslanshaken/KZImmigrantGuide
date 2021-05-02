6937b9cb0f642f3b7250d33ffddf44b854cb6551
asikegoi <aslanshaken@gmail.com>
2021-05-02 10:20:48 -0400

    add to listings

--- client/src/screens/AccountListings/AccountListings.jsx
+++ client/src/screens/AccountListings/AccountListings.jsx
@@ -3,25 +3,44 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getAllJobsForEmployee } from '../../services/getEmployees'
+import { getAllPostsByEmployee } from '../../services/postByEmployees'
 
 export default function AccountListings(props) {
     const { currentUser } = props
     const history = useHistory();
+
+    // Get all lists from back end
     const [jobs, setJobs] = useState([])
-    console.log(jobs)
+    const [jobsByEmployee, setJobsByEmployee] = useState([])
+    console.log(jobsByEmployee)
+
     useEffect(() => {
         const fetchJobs = async () => {
             const jobsList = await getAllJobsForEmployee();
             setJobs(jobsList);
         }
+        const fetchJobsByEmployee = async () => {
+            const jobsByEmployeeList = await getAllPostsByEmployee();
+            setJobsByEmployee(jobsByEmployeeList);
+        }
 
         fetchJobs();
+        fetchJobsByEmployee();
     }, [])
+    // Ends
 
     function filterDate(str) {
         const mdy = str.split('T')[0]
         const date = mdy.split('-')[1] + '-' + mdy.split('-')[2] + '-' + mdy.split('-')[0]
         return date
+    }
+
+    function maxLength(str) {
+        if (str.length > 25) {
+            return str.split('').slice(0, 35).join('') + '.......'
+        } else {
+            return str
+        }
     }
 
     return (
@@ -48,7 +67,26 @@ export default function AccountListings(props) {
                                         <p> {job.job_name}</p>
                                         <p>Post created: {filterDate(job.created_at)}</p>
                                         <p>Last updated: {filterDate(job.updated_at)}</p>
-                                        <p> Category: "Available Jobs" </p>
+                                        <p> Category: " Available Jobs " </p>
+                                        <div className="listings-box-button">
+                                            <button>Edit</button>
+                                            <button>Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    })}
+                    {jobsByEmployee.map((jobByEmployee) => {
+                        if (jobByEmployee.user_id == currentUser?.user.id) {
+                            return (
+                                <div className="listing-box">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcjPkVfPcEjvvIXZIqx30gzFi0Li7WaML4RQ&usqp=CAU" />
+                                    <div>
+                                        <p> {maxLength(jobByEmployee.title)}</p>
+                                        <p>Post created: {filterDate(jobByEmployee.created_at)}</p>
+                                        <p>Last updated: {filterDate(jobByEmployee.updated_at)}</p>
+                                        <p> Category: " I'm looking for a job " </p>
                                         <div className="listings-box-button">
                                             <button>Edit</button>
                                             <button>Delete</button>
@@ -59,20 +97,8 @@ export default function AccountListings(props) {
                         }
                     })}
 
-                    <div className="listing-box">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcjPkVfPcEjvvIXZIqx30gzFi0Li7WaML4RQ&usqp=CAU" />
-                        <div>
-                            <p>Title: </p>
-                            <p>Post created: </p>
-                            <p>Last updated: </p>
-                            <p> Category: "Available Jobs" </p>
-                            <div className="listings-box-button">
-                                <button>Edit</button>
-                                <button>Delete</button>
-                            </div>
-                        </div>
-                    </div>
-                    
+
+
 
                 </div>
 
